1cacdecaf30f047411ec1b509d0f5363e9b27644
ABacic <user@ABC.fritz.box>
2019-07-08 15:21:52 +0200

    made changes to the file

--- myMobileNet_video/sketch.js
+++ myMobileNet_video/sketch.js
@@ -16,13 +16,14 @@ function setup() {
 	 myMobileNet.classify(myVideo, gotResults);
 // this code retains it in one sentence rather than running writing  if the '...' was not there
 	 myDiv = createDiv('...');
+	 myDiv.parent('#wrapper');
 }
 
 function gotResults(err, results) {
 	if (err) console.log(err);
 	if (results) {
 //		console.log(results);
-		myDiv.html(`Label: ${results[0].label}, Confidence: ${results[0].confidence}`);
+		myDiv.html(`The MobileNet model labelled this as: ${results[0].label}, with a confidence of ${results[0].confidence}`);
 		myMobileNet.classify(myVideo, gotResults);
 // this code slows down the recurrence of the results down to 1 second, 1000 milliseconds		
 		setTimeout(() => myMobileNet.classify(myVideo, gotResults), 1000);
